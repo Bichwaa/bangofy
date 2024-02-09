ebe606387dff4a36f38fe85fe0f1f8937279c31b
bichwaa <ptonny13@gmail.com>
2024-02-09 10:14:32 +0300

    incomp

---
+++ components/PackageCard.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="border  p-4 flex flex-col rounded-md" :class="isActive?'border-[#214bf6]':'border-[#939393]'">
+    
+    <h3 class="text-center text-lg font-medium" :class="isActive?'text-[#214bf6]':'text-[#262626]'">
+      {{ title }}
+    </h3>
+
+    <div class="flex gap-3 my-2" v-for="perk, idx in perks" :key="idx">
+      <IconsCheckMark  current-color="#00B400"/>
+      <span class="text-[#3d3c3c]">{{ perk }}</span>
+    </div>
+
+    <div class="grid place-items-center my-2">
+      <span class="font-semibold text-[#3d3c3c]">{{ price }}</span>
+    </div>
+
+    <div class="my-2 grid place-items-center">
+      <button 
+        class="text-xs font-semibold py-2 px-4  rounded-md hover:bg-[#214bf6] hover:text-white hover:border-none duration-300" 
+        :class="isActive?'bg-[#214bf6] text-white border-none':'border-[#939393] text-[#3d3c3c] border'"
+        @click.prevent
+        >{{ isActive?'selected':'Select package' }}</button>
+    </div>
+
+  </div>
+</template>
+
+<script lang="ts" setup>
+import {ref} from 'vue';
+
+
+defineProps({
+  perks:{
+    default: [ "lorem ipsum sijui mengine ni nini", "what is love?", "baby don't hurt me no more", "pure liquid energy, no dash"],
+    type: Array
+  },
+  title:{
+    default:'retail',
+    type: String
+  },
+  price:{
+    default:'TZS 0.0',
+    type:String
+  },
+   isActive:{
+    default:false,
+    type:Boolean
+   }
+})
+
+
+
+
+
+</script>
+
+<style>
+
+</style>
--- pages/post-ad.vue
+++ pages/post-ad.vue
@@ -1,13 +1,120 @@
 <template>
-  <div class="border border-[#3e4e707d] rounded-md p-3 flex flex-col">
-    <h3 class=""></h3>
+  <div class="flex flex-col lg:px-72">
+    <h3 class="font-semibold text-xl p-2 my-2">Post Ad </h3>
+    <form class="border border-[#3e4e707d] rounded-md p-3 flex flex-col mx-2">
+      <h3 class="font-semibold text-xs my-2">Upload images</h3>
+      <div id="img-container" class="flex gap-2 overflow-x-scroll ">
+        <div :class="activeImg=='1st'? 'border-2 border-dotted border-[#214bf6] p-4':'border-2 border-dotted border-[#939393] p-4'">
+          <IconsImageIcon class="w-24 h-24"/>
+        </div>
+
+        <div :class="activeImg=='2nd'? 'border-2 border-dotted border-[#214bf6] p-4':'border-2 border-dotted border-[#939393] p-4'">
+          <IconsImageIcon class="w-24 h-24" :fill-color="activeImg=='2nd'?'':'#64748b '"/>
+        </div>
+
+        <div :class="activeImg=='3rd'? 'border-2 border-dotted border-[#214bf6] p-4':'border-2 border-dotted border-[#939393] p-4'">
+          <IconsImageIcon class="w-24 h-24"   :fill-color="activeImg=='3rd'?'':'#64748b '"/>
+        </div>
+
+        <div :class="activeImg=='4th'? 'border-2 border-dotted border-[#214bf6] p-4':'border-2 border-dotted border-[#939393] p-4'">
+          <IconsImageIcon class="w-24 h-24" :fill-color="activeImg=='4th'?'':'#64748b '"/>
+        </div>
+      </div>
+      <div class="col-span-12 md:col-span-6 flex flex-col my-3">
+        <label for="title" class="font-medium pl-1 text-xs mb-1">Title</label>
+        <input type="text" name="title" class=" border border-1 border-[#939393] rounded-md lg:h-8 w-full">
+      </div>
+
+      <div class="grid grid-cols-12 gap-2 items-center">
+        <div class="col-span-12 md:col-span-6 flex flex-col my-1">
+          <label for="firstname" class="font-medium pl-1 text-xs mb-1">Main category</label>
+          <select name="cars" id="cars" class=" border border-1 border-[#939393] bg-transparent rounded-md lg:h-8 w-full text-xs py-1 px-2 text-gray-700">
+            <option value="one" class="text-xs text-[#939393]">category 1</option>
+            <option value="two" class="text-xs text-[#939393]">category 2</option>
+            <option value="three" class="text-xs text-[#939393]">category 3</option>
+            <option value="four" class="text-xs text-[#939393]">category 4</option>
+          </select>
+        </div>
+
+        <div class="col-span-12 md:col-span-6 flex flex-col my-1">
+          <label for="firstname" class="font-medium pl-1 text-xs mb-1">Subcategory</label>
+          <select name="cars" id="cars" class=" border border-1 border-[#939393] bg-transparent rounded-md lg:h-8 w-full text-xs py-1 px-2 text-gray-700">
+            <option value="one" class="text-xs text-[#939393]">subcategory 1</option>
+            <option value="two" class="text-xs text-[#939393]">subcategory 2</option>
+            <option value="three" class="text-xs text-[#939393]">subcategory 3</option>
+            <option value="four" class="text-xs text-[#939393]">subcategory 4</option>
+          </select>
+        </div>
+      </div>
+
+      <div class="flex flex-col my-3">
+        <p for="price" class="font-medium pl-1 text-xs mb-1">Item Condition</p>
+        <div class="flex items-center gap-8">
+          <div class="flex gap-2">
+          <input type="radio" checked id="html" name="condition" value="New" class=" border border-1 border-[#939393]">
+          <label for="html">New</label>
+        </div>
+        <div class="flex gap-2">
+          <input type="radio" id="html" name="condition" value="New" class=" border border-1 border-[#939393]">
+          <label for="html">Used</label>
+        </div>
+        </div>
+      </div>
+
+      <div class="flex flex-col my-3">
+        <label for="description" class="font-medium pl-1 text-xs mb-1">Description</label>
+        <textarea name="description" placeholder="add item description" class=" border border-1 border-[#939393] rounded-md lg:h-8 w-full p-2 text-sm"></textarea>
+      </div>
+
+      <div class="flex flex-col my-3">
+        <label for="price" class="font-medium pl-1 text-xs mb-1">Price</label>
+        <input type="text" name="price" class=" border border-1 border-[#939393] rounded-md lg:h-8 w-full">
+      </div>
+
+      <div class="flex flex-col my-3">
+        <p for="price" class="font-medium pl-1 text-xs mb-1">Negotiable</p>
+        <div class="flex items-center gap-8">
+          <div class="flex gap-2">
+          <input type="radio" checked id="html" name="condition" value="New" class=" border border-1 border-[#939393]">
+          <label for="html">Yes</label>
+        </div>
+        <div class="flex gap-2">
+          <input type="radio" id="html" name="condition" value="New" class=" border border-1 border-[#939393]">
+          <label for="html">No</label>
+        </div>
+        </div>
+      </div>
+
+      <div class="flex flex-col my-3">
+        <p class="font-medium pl-1 text-xs mb-1">Select package</p>
+        <div class="flex flex-col gap-4 md:flex-row md:gap-8 md:flex-wrap">
+          <PackageCard/>
+          <PackageCard/>
+          <PackageCard :is-active="true"/>
+        </div>
+      </div>
+
+      <button class="rounded-md bg-[#214bf6] w-full text-white py-2 px-4">Next</button>
+    </form>
   </div>
 </template>
 
 <script lang="ts" setup>
+import {ref} from 'vue';
 
+type ActiveImage = "1st" | "2nd" | "3rd" | "4th"
+const activeImg:Ref<ActiveImage> = ref("1st")
 </script>
 
 <style>
+ /* Hide scrollbar for Chrome, Safari and Opera */
+ #img-container::-webkit-scrollbar {
+  display: none;
+}
 
+/* Hide scrollbar for IE, Edge and Firefox */
+#img-container {
+  -ms-overflow-style: none;  /* IE and Edge */
+  scrollbar-width: none;  /* Firefox */
+} 
 </style>
