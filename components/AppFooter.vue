32e059188fafbf9e1c44d68a0d54874249d284b6
bichwaa <ptonny13@gmail.com>
2024-03-07 14:18:42 +0300

    footer

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -1,51 +1,58 @@
 <template>
-    <div class="sticky flex flex-col lg:grid lg:grid-cols-4 gap-8 bg-[#262626] top-0 w-full min-h-64 px-4 lg:px-24 py-4">
+    <div class="sticky flex flex-col lg:grid lg:grid-cols-4 gap-8 lg:gap-4 bg-[#262626] top-0 w-full min-h-64 px-4 lg:px-24 py-4">
       <div class="flex flex-col">
-        <IconsAppLogo theColor="white" class="w-24 h-24" />
+        <IconsAppLogo theColor="white" class="w-24 h-24 cursor-pointer" />
 
-        <p class="text-white lg:w-1/2">
+        <p class="text-white text-sm lg:w-2/3">
           Discover a wide range of classified ads on Mkebe, your go-to destination 
           for buying and selling items in your local community.</p>
+
+        <div class="flex gap-6 mt-6">
+          <LazyIconsYoutubeIcon class="w-5 h-5 cursor-pointer"/>
+          <IconsWhatsappIcon class="w-5 h-5 cursor-pointer"/>
+          <IconsFacebookIcon currentColor="#262626" class="w-5 h-5 cursor-pointer"/>
+          <IconsInstagramIcon class="w-5 h-5 cursor-pointer"/>
+        </div>
       </div>
 
       <div class="flex flex-col items-start lg:pt-8">
-        <h3 class="font-semibold text-white">
+        <h3 class="font-semibold text-white text-sm">
           Categories
         </h3>
 
         <ul class="grid place-items-start">
-          <li class="text-white">Vehicles</li>
-          <li class="text-white">Smartphones</li>
-          <li class="text-white">Multimedia</li>
-          <li class="text-white">Men's fashion</li>
-          <li class="text-white">Women's fashion</li>
-          <li class="text-white">Home</li>
-          <li class="text-white">health & beauty</li>
+          <li class="text-white text-sm my-2 cursor-pointer">Vehicles</li>
+          <li class="text-white text-sm my-2 cursor-pointer">Smartphones</li>
+          <li class="text-white text-sm my-2 cursor-pointer">Multimedia</li>
+          <li class="text-white text-sm my-2 cursor-pointer">Men's fashion</li>
+          <li class="text-white text-sm my-2 cursor-pointer">Women's fashion</li>
+          <li class="text-white text-sm my-2 cursor-pointer">Home</li>
+          <li class="text-white text-sm my-2 cursor-pointer">health & beauty</li>
         </ul>
       </div>
 
       <div class="flex flex-col items-start lg:pt-8">
-        <h3 class="font-semibold text-white">
+        <h3 class="font-semibold text-white text-sm">
           Quick access
         </h3>
 
         <ul class="grid place-items-start">
-          <li class="text-white">Browse ads</li>
-          <li class="text-white">Terms and conditions</li>
-          <li class="text-white">Privacy policy</li>
+          <li class="text-white text-sm my-2 cursor-pointer">Browse ads</li>
+          <li class="text-white text-sm my-2 cursor-pointer">Terms and conditions</li>
+          <li class="text-white text-sm my-2 cursor-pointer">Privacy policy</li>
         </ul>
 
       </div>
 
       <div class="flex flex-col items-start lg:pt-8">
-        <h3 class="font-semibold text-white">
+        <h3 class="font-semibold text-white text-sm">
           Contact us
         </h3>
 
         <ul class="grid place-items-start">
-          <li class="text-white">+255658094941</li>
-          <li class="text-white">info@bangofy.co.tz</li>
-          <li class="text-white">Dar-es-salaaam, Tanzania</li>
+          <li class="text-white text-sm my-2">+255658094941</li>
+          <li class="text-white text-sm my-2">info@bangofy.co.tz</li>
+          <li class="text-white text-sm my-2">Dar-es-salaaam, Tanzania</li>
         </ul>
 
       </div>
