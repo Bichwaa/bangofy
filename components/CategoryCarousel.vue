c4819614c8d7bfc928522742149672319da6ea3c
bichwaa <ptonny13@gmail.com>
2024-03-08 15:19:00 +0300

    header logo size

--- components/CategoryCarousel.vue
+++ components/CategoryCarousel.vue
@@ -2,7 +2,7 @@
     <div class="carou2 mt-12">
       <!-- ON LARGE DEVICES ONLY -->
         <div class="hidden lg:flex gap-8 ">
-          <Card v-for="(val, i) in categoryNames" :key="i" class="border-0 shadow-none bg-[#E4E9FE] mx-0 rounded-md w-[14vh] h-[14vh] flex items-center justify-center cursor-pointer">
+          <Card v-for="(val, i) in categoryNames" :key="i" class="border-0 m--0 p-2 shadow-none bg-[#E4E9FE] mx-0 rounded-md w-[14vh] h-[14vh] flex items-center justify-center cursor-pointer">
             <CardContent class="p-0 border-0 shadow-none">
               <div class="flex flex-col items-center">
                 <!-- <div class="grid place-items-center bg-blue-600 rounded-md m-2 p-2">
@@ -21,7 +21,7 @@
           <CarouselContent class="flex">
             <!-- <transition-group name="slide-fade"> -->
               <CarouselItem v-for="(val, i) in categoryNames" :key="i" class="mr-2 gap-2 basis-[31%] md:basis-[19%] lg:basis-[15%] xl:basis-[8%] cursor-pointer">
-                <Card class="mx-1 border-0 shadow-none bg-[#E4E9FE] rounded-md p-1 w-[12vh] h-[12vh] flex items-center justify-center">
+                <Card class="mx-1 border-0 shadow-none bg-[#E4E9FE] rounded-md p-1 w-[12vh] max-h-[12vh] flex items-center justify-center">
                   <CardContent class="p-0 border-0 shadow-none">
                     <div class="flex flex-col items-center pt-1">
                       <!-- <div class="grid place-items-center bg-blue-600 rounded-md m-2 p-2">
@@ -29,7 +29,7 @@
                       </div> -->
                       <NuxtImg class=""  :src="val.url"/>
                       <div class="grid place-items-center text-center my-4 font-medium">
-                        {{ val.name }}
+                        {{ addLineBreaks(val.name) }}
                       </div>
                     </div>
                   </CardContent>
@@ -61,10 +61,15 @@ interface Category {
 
 const categoryNames: Array<Category>= [
 { name:"Vehicles", url:"/img/vehicles.png"}, { name:"Smartphones", url:"/img/smartphones.png"},
-{ name:"Electronics", url:"/img/electronics.png"}, { name:"Mens's fashion", url:"/img/men-fashion.png"},
+{ name:"Electronics", url:"/img/electronics.png"}, { name:"Mens's\n fashion", url:"/img/men-fashion.png"},
 { name:"Women's fashion", url:"/img/women-fashion.png"}, { name:"Home", url:"/img/home.png"},
 { name:"Health & beauty", url:"/img/health-beauty.png"}, { name:"Kids", url:"/img/kids.png"},
 { name:"Hobbies & Sports", url:"/img/hobby-sports.png"}, { name:"Jobs", url:"/img/jobs.png"},
 { name:"Real Estate", url:"/img/real-estate.png"}, { name:"Others", url:"/img/others.png"},
 ]
+
+function addLineBreaks(str:any) {
+  return str.replace(/\s/g, '\n');
+}
+
 </script>
